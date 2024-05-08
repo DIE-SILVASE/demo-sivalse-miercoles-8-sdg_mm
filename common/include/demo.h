7162e0ef7966385c466be7eb2f87cce9bcff49bc
Monica Ferrer Gomez-Cano <monica.ferrer@alumnos.upm.es>
2024-05-08 13:25:02 +0200

    actualizacion demo.h

--- common/include/demo.h
+++ common/include/demo.h
@@ -1,8 +1,3 @@
 #include <inttypes.h>
 
-<<<<<<< HEAD
 int32_t suma(int32_t num1, int32_t num2);
-=======
-// Función suma
-int32_t suma(int32_t num1, int32_t num2);
->>>>>>> 0b122527beb14e4a3c614967822dee65a51ac148
